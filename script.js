7faa2e841ef93dca3a1abe66eed3034a46abdd40
VitorPaulon <vitorpaulon@gmail.com>
2022-08-30 20:30:47 -0300

    Atualização 30-08-2022

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function verResultado(){
-    var palpite = document.getElementById('palpite').value;
-    var numSort = Math.floor(Math.random() * 10)
+    const palpite = document.getElementById('palpite').value;
+    const numSort = Math.floor(Math.random() * 10)
     const resultado = document.querySelector('p');
+    
     if( palpite == numSort){
         resultado.textContent = ('Voce ganhou!')
     }else{
         resultado.textContent = ('Voce perdeu! O numero era ' + numSort + '. Tente de novo.')
+        }
     }
-}  
+ 
 function jogarDeNovo(){
-    var numSort = Math.floor(Math.random() * 10)
+    const numSort = Math.floor(Math.random() * 10)
 
 }
 function JogarDeNovo(){
